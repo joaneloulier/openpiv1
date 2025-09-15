9afd08948c767698f6c60435ff8a4c689bfb09ca
Joane Loulier <joane.loulier@centrale-med.fr>
2025-09-15 15:30:01 +0200

    fini le tuto, je connais à peu près les fonctions dispos

--- tutorial.py
+++ tutorial.py
@@ -8,8 +8,8 @@ import pathlib
 
 path = importlib_resources.files('openpiv')
 
-frame_a=tools.imread('images_openpiv/B005_1.tif')
-frame_b=tools.imread('images_openpiv/B005_2.tif')
+frame_a=tools.imread('images_openpiv/B001_1.tif')
+frame_b=tools.imread('images_openpiv/B001_2.tif')
 
 fig,ax = plt.subplots(1,2,figsize=(12,10))
 ax[0].imshow(frame_a,cmap=plt.cm.gray)
@@ -37,3 +37,58 @@ u0, v0, sig2noise = pyprocess.extended_search_area_piv(
     sig2noise_method='peak2peak',
 )
 
+#3 arrays returned : u component of the velocity vector /
+#v component of the velocity vector / the s2n ratio
+
+x, y = pyprocess.get_coordinates(
+    image_size=frame_a.shape,
+    search_area_size=searchsize,
+    overlap=overlap
+)
+
+#POST PROCESSING
+#sig2noise_val : get a mask to indicate which vectors have a minimum amount
+#oh s2n. Vectors below a certain threshol substituted by NaN
+
+invalid_mask=validation.sig2noise_val (
+    sig2noise,
+    threshold=1.05,
+)
+
+#replace_outliers : to find outlier vectors and substitute them by an 
+#average of neighbouring vectors
+#the larger the kernel size, the larger the neighbourhood considered
+#the amount of iterations can be chosen via max_iter
+
+u2, v2 = filters.replace_outliers(
+    u0, v0,
+    invalid_mask,
+    method='localmean',
+    max_iter=3,
+    kernel_size=3,
+)
+
+# convert x,y to mm
+# convert u,v to mm/sec
+
+x, y, u3, v3 = scaling.uniform(
+    x, y, u2, v2,
+    scaling_factor = 96.52,  # 96.52 pixels/millimeter
+)
+
+# 0,0 shall be bottom left, positive rotation rate is counterclockwise
+x, y, u3, v3 = tools.transform_coordinates(x, y, u3, v3)
+
+#RESULTS
+
+tools.save('exp1_001.txt' , x, y, u3, v3, invalid_mask)
+
+fig, ax = plt.subplots(figsize=(8,8))
+tools.display_vector_field(
+    pathlib.Path('exp1_001.txt'),
+    ax=ax, scaling_factor=96.52,
+    scale=50, # scale defines here the arrow length
+    width=0.0035, # width is the thickness of the arrow
+    on_img=True, # overlay on the image
+    image_name= str(path / 'data'/'test1'/'exp1_001_a.bmp'),
+);
